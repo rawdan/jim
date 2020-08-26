09519fe481dc3b7a0520a0c2e155b04e496b6cae
Bogdan Crudu <bogdancrudu@yahoo.com>
2020-08-26 19:30:42 +0300

    make it green

--- spec/request/workouts_request_spec.rb
+++ spec/request/workouts_request_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe 'Workouts', type: :request do
   describe 'show workout/:id' do
 
     it 'returns success' do
-      get '/api/v1/workouts/1'
+      get '/api/v1/workouts/1.:json'
 
-      expect(response).to have_http_status 200
-
+      expect(response).to have_http_status :success
     end
   end
 end
